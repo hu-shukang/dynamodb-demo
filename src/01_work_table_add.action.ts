7c0c2049502f297ecb66142334d3233159cfbed9
HU SHUKANG <hushukang@gmail.com>
2023-01-16 18:45:55 +0900

    コメント追加

--- src/01_work_table_add.action.ts
+++ src/01_work_table_add.action.ts
@@ -1,18 +1,23 @@
 import { PutCommand, PutCommandInput } from '@aws-sdk/lib-dynamodb';
 import { docClient } from './utils/dynamodb.util';
 
+/**
+ * 勤務表テーブルにデータを挿入するAction
+ */
 class WorkTableAddAction {
   public async action() {
+    // 1. 挿入データを用意する
     const input: PutCommandInput = {
       TableName: 'DynamoDB-Demo-WorkTable',
       Item: {
         userId: 'A001',
-        date: '2023-02-01',
+        date: '2023-01-20',
         startTime: '09:00',
         endTime: '18:00',
         break: 60,
       },
     };
+    // 2. APIを通して挿入する
     const result = await docClient.send(new PutCommand(input));
     return result;
   }
