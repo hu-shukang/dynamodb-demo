05859c48c43ee27572f848dc9ff161661ca82aad
HU SHUKANG <hushukang@gmail.com>
2023-01-18 20:19:40 +0900

    add comment

--- src/04_work_table_delete.action.ts
+++ src/04_work_table_delete.action.ts
@@ -1,8 +1,12 @@
 import { DeleteCommand, DeleteCommandInput } from '@aws-sdk/lib-dynamodb';
 import { docClient } from './utils/dynamodb.util';
 
+/**
+ * 勤務表テーブルからデータを削除するAction
+ */
 class WorkTableDeleteAction {
   public async action() {
+    // 1. テーブル名と削除したいデータのキー情報を設定する
     const input: DeleteCommandInput = {
       TableName: 'DynamoDB-Demo-WorkTable',
       Key: {
@@ -10,6 +14,7 @@ class WorkTableDeleteAction {
         date: '2023-01-01',
       },
     };
+    // 2. APIを通して削除する
     const result = await docClient.send(new DeleteCommand(input));
     return result;
   }
